63eb21ffe232e8043d961058832eca1867c79517
NishantBadlani <nishant.03badlani@gmail.com>
2020-12-17 23:18:47 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import RegisterForm from "./Register";
 
-const App = () => {
+class App extends Component {
+  render(){
   return (
-    <div id="main"></div>
+    <div id="main">
+    <RegisterForm /> 
+    </div>
   )
+  }
 }
 
 
 export default App;
+export const Register= RegisterForm;
